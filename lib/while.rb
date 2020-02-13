75c4efe2028e5afa02dd4e838ec1a47e5f0b7345
Todd Norton <toddmnorton@live.com>
2020-02-13 20:36:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
   levitation_force = 6
   while levitation_force < 10 
-  puts "You should increment your levitation force by 1."
+    puts "You should increment your levitation force by 1."
   levitation_force += levitation_force
   #your code here
 end
